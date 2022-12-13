dbe5fef8fac5e9f413756ec54b6c8317461cb5e2
steamCorn <64536303+steamCorn@users.noreply.github.com>
2022-12-13 17:40:12 +0100

    Updates

---
+++ precache-manifest.976a6e5f752fbc71664827c37bd1f09b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7b87666ee6c5d43e47b",
+    "url": "/Portfolio/static/css/main.030389a3.chunk.css"
+  },
+  {
+    "revision": "c7b87666ee6c5d43e47b",
+    "url": "/Portfolio/static/js/main.c7b87666.chunk.js"
+  },
+  {
+    "revision": "abe40401b57467cf7805",
+    "url": "/Portfolio/static/js/1.abe40401.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "e85a1d36468cf8286a4ac07903d35690",
+    "url": "/Portfolio/index.html"
+  }
+];
